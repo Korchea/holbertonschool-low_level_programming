cc6b7cf6883d9beea2e7823f1f9695371c2d6a24
root <guilllermo.vega@gmail.com>
2023-02-15 09:13:26 -0800

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main () {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return (0);
+/**
+ * main - Entry point
+ * Description: 'Print using puts'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
