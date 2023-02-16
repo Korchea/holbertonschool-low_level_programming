cbf461e9aa9b774f2ebeff3137ea09adb69725ba
root <guilllermo.vega@gmail.com>
2023-02-16 06:26:28 -0800

    Task 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: 'Prints the lowercase alphabet in reverse,
+ * followed by a new line.'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-int i;
-for (i = 122; i > 96; i--)
-{
-	putchar(i);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
