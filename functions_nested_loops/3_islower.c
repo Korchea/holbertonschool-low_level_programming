7d03b910a0e62e5fabe02a9d365a408c146f6a9a
root <guilllermo.vega@gmail.com>
2023-02-16 11:08:51 -0800

    Task 3

---
+++ functions_nested_loops/3_islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: 'Checks for lowercase character.'
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
