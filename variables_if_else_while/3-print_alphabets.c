c861af3f5f38f1735d57b46d4264a0050bc35e5c
root <guilllermo.vega@gmail.com>
2023-02-16 06:01:22 -0800

    Task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: 'Prints the alphabet in lowercase
+ * and uppercase, followed by a new line.'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int i;
-	char a[54] = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-	for (i = 0; i < 54; i++)
+
+	for (i = 97; i < 123; i++)
 	{
-		putchar(a[i]);
+		if (i > 96 && i < 123)
+		{
+			putchar(i);
+		}
+	}
+	for (i = 65; i < 91; i++)
+	{
+		if (i > 64 && i < 91)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
