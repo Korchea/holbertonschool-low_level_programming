663dc1a16b1be3c81c9023f1b76e18c2fdafcedf
root <guilllermo.vega@gmail.com>
2023-02-16 06:07:15 -0800

    Task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: 'Prints the alphabet in lowercase
+ * except q and e, followed by a new line.'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-int i;
-for (i = 97; i < 123; i++)
-{
-	if (i != 101 || i != 113)
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 || i != 113)
 	{
 		putchar(i);
 	}
-}
-putchar('\n');
-return (0);
+	}
+	putchar('\n');
+	return (0);
 }
