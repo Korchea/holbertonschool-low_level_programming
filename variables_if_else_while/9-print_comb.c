a5b4346dc4406dcce52a9f14cc8c4e63763118d1
root <guilllermo.vega@gmail.com>
2023-02-16 07:16:28 -0800

    Task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: 'Prints all possible combinations
+ * of single-digit numbers.'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i--)
 	{
 		putchar(i);
