97415b4a5cb4b135522b1a90111f78b280565aa4
root <guilllermo.vega@gmail.com>
2023-02-17 04:43:30 -0800

    Task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,17 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n < 99; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		for (; n > 97; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
 	}
-	_putchar('\n');
+	printf("%d\n", n);
 }
