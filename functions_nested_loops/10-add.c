36500f8030c5c9f07dec5d8f9c1337e50f7fe5dd
root <guilllermo.vega@gmail.com>
2023-02-17 04:08:01 -0800

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: 'Adds two integers and returns the result.'
+ * @i: is an int
+ * @j: is an int
+ * Return: Always i + j (Success)
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
