fa163a3d6ca2718552f3b66c65a9a871689afabd
root <guilllermo.vega@gmail.com>
2023-02-17 06:10:09 -0800

    Task 7

---
+++ hello_world/algo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b = 2;
+	a = 0;
+	for (; a < b;)
+	{
+		    printf("%d\n", a++);
+	}
+	return (0);
+}
