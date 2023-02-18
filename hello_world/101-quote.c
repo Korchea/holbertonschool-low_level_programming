14598ea6a7bf6f4204ad095eaa8002a2a5e3b1d8
root <guilllermo.vega@gmail.com>
2023-02-18 11:41:49 -0800

    Task 7

---
+++ hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * 
+ *    * Description: 'the program's description'
+ *     * 
+ *      * Return: Always 0 (Success)
+ *       */
+
+int main(void)
+{
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (i = 0; i < 30; i++)
+	{
+		write(2 , a[i], );
+	}
+	return (1);
+}
