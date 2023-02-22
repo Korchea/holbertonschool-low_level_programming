28b4f38ea6df235bda1361d115e3a8b7f089cf5c
root <guilllermo.vega@gmail.com>
2023-02-22 08:49:10 -0800

    Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Check the code.
+ * Description: 'Prints the numbers, from 0 to 9, exept 2 and 4,
+ * followed by a new line.'
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ((i != 50) && (i != 52))
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
