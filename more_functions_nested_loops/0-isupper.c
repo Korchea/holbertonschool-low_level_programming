1371b865d4c62b57e5ce0af0003dfd9e8c13e53d
root <guilllermo.vega@gmail.com>
2023-02-22 07:16:08 -0800

    Task 1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper checks for uppercase character.
+ * _isupper - checks the code
+ * Description: 'Checks for uppercase character.'
  * @c: is a char.
  * returns 1 if c is uppercase, 0 otherwise.
  */
