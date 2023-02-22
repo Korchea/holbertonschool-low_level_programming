094a035cd5a3138fa836e846ee5a12722ecd41d9
root <guilllermo.vega@gmail.com>
2023-02-22 10:20:17 -0800

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,31 +10,32 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	printf("1");
+	for (i = 2; i < 101; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
 			if ((i % 3) == 0)
 			{
-				printf("Fizz ");
+				printf(" Fizz");
 			}
 			else
 			{
 				if ((i % 5) == 0)
 				{
-					printf("Buzz ");
+					printf(" Buzz");
 				}
 				else
 				{
-					printf("%d ", i);
+					printf(" %d", i);
 				}
 			}
 		}
 	}
-	printf("\n");
+	printf(" \n");
 	return (0);
 }
