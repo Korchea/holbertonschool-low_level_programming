c92f956ba691ce14479b26902930f216ee3ed962
root <guilllermo.vega@gmail.com>
2023-02-28 08:38:06 -0800

    Task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - Check the code.
+ * Descripion: 'Concatenate two strings.'
+ * @dest: is a string.
+ * @src: is a string.
+ * @n: is an int.
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (j >= 0)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+		{
+			break;
+		}
+		if (j > (n - 2))
+		{
+			break;
+		}
+		i++;
+		j++;
+	}
+	return (dest);
+}
