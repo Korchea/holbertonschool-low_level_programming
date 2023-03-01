c4a4a1a5b5d2a78d6138ec392372dedacc8a98d3
root <guilllermo.vega@gmail.com>
2023-03-01 05:57:24 -0800

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string -Check the code.
+ * Decription: 'Capitalizes all words of a string.'
+ * @s: is a string.
+ * Return: s.
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	char j, k;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		j = *(s + i);
+		if (i == 0)
+		{
+			*(s + i) = toupper(j);
+		}
+		else
+		{
+			k = *(s + (i - 1));
+			if (k == 9 || k == 44 || k == 46 || k == 59 || k == 63)
+			{
+				*(s + i) = toupper(j);
+			}
+			else
+			{
+				if ((k >= 32 && k <= 34) || k == 40 || k == 41)
+				{
+					*(s + i) = toupper(j);
+				}
+				else
+				{
+					if (k == 123 || k == 125)
+					{
+						*(s + i) = toupper(j);
+					}
+					else
+					{
+						if (k == '\n')
+						{
+							*(s + i) = toupper(j);
+						}
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
