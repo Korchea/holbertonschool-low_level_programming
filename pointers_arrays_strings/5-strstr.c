29f8af20fedfc51755dc324f3b87ec32831e92c9
root <guilllermo.vega@gmail.com>
2023-03-02 08:33:56 -0800

    Task 7

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - Check the code.
+ * Description: 'Locates a substring.'
+ * @haystack: is a string.
+ * @needle: is a substring.
+ * Return: A pointer to the beginning of the located substring, or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
