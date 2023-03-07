313a3eb03d2664344ba24f197a64a3b8d497be3a
root <guilllermo.vega@gmail.com>
2023-03-07 04:38:26 -0800

    Task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _sqrt - Check the code.
+ * Description: 'If is bigger than 0 do the squertroot.'
+ * @n: Is an integer.
+ * @i: Is an integer equal or bigger than 2.
+ * Return: The value of the sqrt.
+ */
+
+int _sqrt(int n , int i)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+		{
+			return (i);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - Check the code.
+ * Description: 'Returns the natural square root of a number.'
+ * @n: Is an integer.
+ * Return: The square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt(n, 2));
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * prime - Check the code.
+ * Description: 'Detect prime number.'
+ * @n: is an integer.
+ * @c: is an integer.
+ * Return: 1 if it is prime else 0.
+ */
+
+int prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (prime(n, c + 1));
+	}
+}
+/**
+ * is_prime_number - Check the code.
+ * Description: 'Returns 1 if the input integer is a prime number,
+ * otherwise return 0.'
+ * @n: is an integer.
+ * Return: 1 and 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime(n, 2));
+}
