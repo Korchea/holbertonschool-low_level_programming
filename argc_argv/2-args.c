28aa166e48f19300f201226f35fc1fadc2fb273c
root <guilllermo.vega@gmail.com>
2023-03-07 10:44:48 -0800

    Task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Check the code.
+ * Description: 'Prints all arguments it receives.'
+ * @argc: is the number of arguments.
+ * @argv: is the file name and the arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
