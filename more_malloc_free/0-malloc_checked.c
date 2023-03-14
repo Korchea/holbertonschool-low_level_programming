8965fad6b956d600d903e981f313eb651339c7d4
root <guilllermo.vega@gmail.com>
2023-03-14 11:11:24 -0700

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Check the code.
+ * Description: 'Allocates memory using malloc.'
+ * @b: Is a positive integer.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *i;
+
+	i = malloc(b);
+	if (i == NULL)
+	{
+		exit(98);
+	}
+	return (i);
+}
