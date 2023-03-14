e8e58c267cd1d484b6bb537adb847af43d38c5d5
root <guilllermo.vega@gmail.com>
2023-03-14 11:13:55 -0700

    Task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,6 +5,7 @@
  * malloc_checked - Check the code.
  * Description: 'Allocates memory using malloc.'
  * @b: Is a positive integer.
+ * Return: The allocate memory.
  */
 
 void *malloc_checked(unsigned int b)
