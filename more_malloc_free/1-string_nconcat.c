95f1730161a89e5f7fb050ef3418d7fd30af1093
root <guilllermo.vega@gmail.com>
2023-03-15 11:57:45 -0700

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,30 +14,28 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *i;
-	unsigned int k;
+	unsigned int j, k;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
-	else
+	if (s2 == NULL)
 	{
-		if (s1 == NULL)
-		{
-			s1 = "";
-		}
-		if (s2 == NULL)
-		{
-			s2 = "";
-		}
-		k = strlen(s1);
-		i = malloc(sizeof(char) * (k + n + 1));
-		if (i == NULL)
-		{
-			exit(1);
-		}
-		strcat(i, s1);
-		strncat(i, s2, n);
-		return (i);
+		s2 = "";
 	}
+	k = strlen(s1);
+	j = strlen(s2);
+	if (j < n)
+	{
+		n = j;
+	}
+	i = malloc(sizeof(char) * (k + n + 1));
+	if (i == NULL)
+	{
+		exit(1);
+	}
+	strcat(i, s1);
+	strncat(i, s2, n);
+	return (i);
 }
--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,3 +10,10 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+
