8e016f4e147338466243126ba19043d809a3eb9a
root <guilllermo.vega@gmail.com>
2023-03-20 07:24:58 -0700

    Task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Check the code.
+ * Descriptio: 'Initialize a variable of struct dog.'
+ * @struct dog: Is a struct.
+ * @name: Is string.
+ * @age: Is a float.
+ * @owner: Is a string.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
