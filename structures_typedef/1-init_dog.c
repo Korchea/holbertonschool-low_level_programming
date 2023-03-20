dabd63de7f37664bd60827d8123d1aed43000d4d
root <guilllermo.vega@gmail.com>
2023-03-20 07:33:27 -0700

    Task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
 /**
  * init_dog - Check the code.
  * Descriptio: 'Initialize a variable of struct dog.'
- * @struct dog: Is a struct.
+ * @d: Is a struct.
  * @name: Is string.
  * @age: Is a float.
  * @owner: Is a string.
@@ -12,11 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-	{
-		printf("Ok");
-	}
-	else
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
