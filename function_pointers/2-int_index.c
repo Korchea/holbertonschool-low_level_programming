3650dc1dd30adddd764ab251751f7bc8d6ee34af
root <guilllermo.vega@gmail.com>
2023-03-22 06:59:55 -0700

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array != NULL && cmp != NULL)
 	{
-		for (i = 0; i < size; )
+		for (i = 0; i <= size; )
 		{
 			if (cmp(array[i]) != 0)
 			{
