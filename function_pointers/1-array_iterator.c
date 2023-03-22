74947bc482bef59c59bbbfb5b6ad5fd9a9e8a7c4
root <guilllermo.vega@gmail.com>
2023-03-22 06:20:52 -0700

    Task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -15,6 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		action(array[i]);
+		if (array[i] != NULL)
+		{
+			action(array[i]);
+		}
 	}
 }
