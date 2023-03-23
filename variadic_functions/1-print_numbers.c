14f0b1e45722f4503a9d70f609421dfb6825a439
root <guilllermo.vega@gmail.com>
2023-03-23 05:40:25 -0700

    Task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,13 +21,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	for (i = 0; i < n; i++)
 	{
-		if (i < n - 1)
+		if (separator == NULL && i < n - 1)
 		{
-			printf("%d%c", va_arg(ap, int), *separator);
+			printf("%d", va_arg(ap, int));
 		}
 		else
 		{
-			printf("%d\n", va_arg(ap, int));
+			if (i < n - 1)
+			{
+				printf("%d%s", va_arg(ap, int), separator);
+			}
+			else
+			{
+				printf("%d\n", va_arg(ap, int));
+			}
 		}
 	}
 	va_end(ap);
