a2ff96d2e8e2ea7928b95fb000ea1ee0612d2360
root <guilllermo.vega@gmail.com>
2023-03-23 10:58:47 -0700

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -30,7 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-	d->name = strcpy(d->name,name);
+	d->name = strcpy(d->name, name);
 	d->age = age;
 	d->owner = strcpy(d->owner, owner);
 	return (d);
