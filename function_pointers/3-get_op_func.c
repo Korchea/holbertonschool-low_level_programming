1a8fbff7bf2b39d1a3ede8956ecc70b0fed814a9
root <guilllermo.vega@gmail.com>
2023-03-23 13:48:50 -0700

    Task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - 'Selects the correct function to perform the operation asked.'
+ * @s: Is the operator passed as argument to the program.
+ * @a: Is an integer.
+ * @b: Is an integer.
+ * Return: A pointer to the punction the corresponds to the operator given.
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL && *ops[i].op != *s)
+	{
+		i++;
+	}
+	if (*ops[i].op != *s)
+	{
+		return (NULL);
+	}
+	return (*ops[i].f);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - 'Conains the main function only.'
+ * @argc: Is the number of strings.
+ * @argv: I a string.
+ * Return: The operation result.
+ */
+
+int main(int argc, char *argv[])
+{
+	char *i, *j, *k;
+
+	i = argv[1];
+	j = argv[2];
+	k = argv[3];
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*j != '+' && *j != '-' && *j != '*' && *j != '/' && *j != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*j == '/' || *j == '%') &&
+			(*i == '0' || *k == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(j)(atoi(i), atoi(k)));
+	return (0);
+}
