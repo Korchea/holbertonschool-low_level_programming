7a670704ed5f3530d13ff98ff01ed2e02d81cf39
root <guilllermo.vega@gmail.com>
2023-04-03 11:17:41 -0700

    Task 4 completed

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - 'Frees a list_t list.'
+ * @head: Is the list to free.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
