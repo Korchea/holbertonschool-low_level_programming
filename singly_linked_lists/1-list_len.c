0bf2dc1a16a3a995223890b264686254e4816722
root <guilllermo.vega@gmail.com>
2023-04-03 09:24:48 -0700

    Task 1 completed

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - 'Returns the number of elements in a linked list_t list.'
+ * @h: Is the list of type list_t.
+ * Returns: The number of elements linked.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
