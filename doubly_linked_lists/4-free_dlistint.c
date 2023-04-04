eb343c26d4d028326e80fe7a0f4abf460f169cf0
root <guilllermo.vega@gmail.com>
2023-04-04 06:50:16 -0700

    Task 4 completed

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - 'Frees a dlistint_t list.'
+ * @head: Is a list.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *aux;
+
+	while (head->prev != NULL)
+		head = head->prev;
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+}
