fbfe4ef3c41389f3b24fe22ecb252a1ba155cea8
root <guilllermo.vega@gmail.com>
2023-04-07 12:01:46 -0700

    Directory, readme and header include.

---
+++ bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef __main_h__
+#define __main_h__
+
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bin(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
