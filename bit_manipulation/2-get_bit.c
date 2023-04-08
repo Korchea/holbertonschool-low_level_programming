7c7b71cfe9798493ae2895e2bb73c661624129cc
root <guilllermo.vega@gmail.com>
2023-04-08 12:17:07 -0700

    Task 2 correction

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,13 +11,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 	for (i = 0; i < index; i++)
 		if (n >> 1)
 			n = n >> 1;
 		else
 			break;
-	if (i == index)
-		return (n & 1);
-	else
-		return (-1);
+	return (n & 1);
 }
