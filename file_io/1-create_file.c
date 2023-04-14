ab5a696386d8af56086dd858f043c9fcaafde7aa
root <guilllermo.vega@gmail.com>
2023-04-14 09:50:09 -0700

    Task 1 completed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,12 +15,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[len] != '\0')
 		len++;
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	cont = write(file, text_content, len);
-	if (cont == -1)
-		return (-1);
-	close (file);
+	if (text_content != NULL)
+	{
+		cont = write(file, text_content, len);
+		if (cont == -1)
+			return (-1);
+	}
+	close(file);
 	return (1);
 }
