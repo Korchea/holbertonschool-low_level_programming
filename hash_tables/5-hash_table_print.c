b065cfbbd19f2ea0eef7a8a423d86caebd8e9614
root <guilllermo.vega@gmail.com>
2023-04-14 07:08:22 -0700

    Task 5 completed

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,17 +10,20 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0;
 	int j = 0;
 
-	printf("{");
-	for (i = 0; ht != NULL && i < ht->size; i++)
+	if (ht != NULL)
 	{
-		while (ht->array[i] != NULL)
+		printf("{");
+		for (i = 0; i < ht->size; i++)
 		{
-			if (j > 0)
-				printf(", ");
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			ht->array[i] = ht->array[i]->next;
-			j = 1;
+			while (ht->array[i] != NULL)
+			{
+				if (j > 0)
+					printf(", ");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				ht->array[i] = ht->array[i]->next;
+				j = 1;
+			}
 		}
+		printf("}\n");
 	}
-	printf("}\n");
 }
