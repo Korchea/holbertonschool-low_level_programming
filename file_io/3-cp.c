4fe0305b9cddcca73c87e32b4b3ee28b7d190949
root <guilllermo.vega@gmail.com>
2023-04-15 09:07:23 -0700

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,9 +9,41 @@
 
 int main(int argc, char *argv[])
 {
+	int o, r, w, c;
+
 	if (argc != 3)
 	{
-		dprintf(1, "Usage: cp file_from file_to");
+		dprintf(1, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	o = open();
+	if (argv[1] == NULL)
+	{
+		dprintf(1, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	o = open(argv[1], O_TRUNC | O_CREAT | O_RDWR, 0664);
+	if (o == -1)
+	{
+		dprintf(1, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	r = read(o, argv[1], 1024);
+	if (r == -1)
+	{
+		dprintf(1, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	w = write(o, argv[2], r);
+	if (w == -1)
+	{
+		dprintf(1, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	c = close(o);
+	if (c == -1)
+	{
+		dprintf(1, "Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+	return (0);
+}
